516dd4a97d8e9825b85920e778c2cd2692d6bab4
JAMIL AHMAD <jameel.shigri@gmail.com>
2026-08-10 13:20:40 +0500

    by id

--- controllers/departmentController.js
+++ controllers/departmentController.js
@@ -11,6 +11,33 @@ exports.getAllDepartments = async (req, res) => {
     const result = await pool.query("Select * From departments");
     sendSuccess(res, 200, result.rows);
   } catch (error) {
+    sendError(res, 500, "Internal Server Error");
+  }
+};
+exports.getDepartById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query(
+      `Select * from departments where dep_id = $1`,
+      [id],
+    );
+    sendSuccess(res, 200, result.rows[0]);
+  } catch (error) {
     console.log(error);
+    sendError(res, 500, "Something went wrong");
+  }
+};
+exports.getTotalStud_Depart = async (req, res) => {
+  try {
+    const result = await pool.query(`SELECT
+    d.dep_id,
+    d.Name AS department_name,
+    COUNT(s.id) AS total_students
+    FROM departments d
+    LEFT JOIN students s ON d.dep_id = s.dep_id
+    GROUP BY d.dep_id, d.Name ORDER BY d.dep_id ASC`);
+    sendSuccess(res, 200, result.rows);
+  } catch (error) {
+    sendError(res, 500, "Internal Server Error");
   }
 };
--- routes/departmentRoutes.js
+++ routes/departmentRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const { getAllDepartments } = require("../controllers/departmentController");
+const {
+  getAllDepartments,
+  getTotalStud_Depart,
+  getDepartById,
+} = require("../controllers/departmentController");
 const router = express.Router();
 
 router.get("/", getAllDepartments);
+router.get("/:id", getDepartById);
+
+router.get("/total-students", getTotalStud_Depart);
 
 module.exports = router;
